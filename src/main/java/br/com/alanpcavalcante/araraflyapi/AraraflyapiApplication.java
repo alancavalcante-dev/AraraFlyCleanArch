450b85f53b87fcd72f66f8c0fa182ac762e03bcf
alancavalcante-dev <ala.pereiradocavalcante@gmail.com>
2025-07-21 13:49:58 -0300

    first commit

---
+++ src/main/java/br/com/alanpcavalcante/araraflyapi/AraraflyapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alanpcavalcante.araraflyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraraflyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AraraflyapiApplication.class, args);
+	}
+
+}
